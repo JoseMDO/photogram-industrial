e0c8751a8d573fdd6bbbccf9337aa6f7c52b58c1
Jose Montes De Oca Morfin <112668683+JoseMDO@users.noreply.github.com>
2024-02-22 19:13:18 +0000

    Added feed, followers, and following.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,11 +3,20 @@ class UsersController < ApplicationController
     @user = User.find_by!(username: params.fetch(:id))
   end
 
-
-
   def liked
     @user = User.find_by!(username: params.fetch(:username))
   end
 
-  
+  def feed
+    @user = User.find_by!(username: params.fetch(:username))
+  end
+
+  def followers
+    @user = User.find_by!(username: params.fetch(:username))
+  end
+
+  def following
+    @user = User.find_by!(username: params.fetch(:username))
+  end
+
 end 
