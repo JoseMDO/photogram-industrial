fd31b40fe5f77a1d92441b891bb62c7dcb72b6cd
Jose Montes De Oca Morfin <112668683+JoseMDO@users.noreply.github.com>
2024-02-22 17:32:51 +0000

    Added link to posts and liked photos

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,12 @@ class UsersController < ApplicationController
   def show
     @user = User.find_by!(username: params.fetch(:id))
   end
+
+
+
+  def liked
+    @user = User.find_by!(username: params.fetch(:username))
+  end
+
+  
 end 
-
-
--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,11 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
+
+
+  get ":username/liked" => "users#liked", as: :liked
+  get ":username" => "users#show", as: :user
+
+
+
 end
